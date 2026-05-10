6f39b1fc348474785a48150e2c2c83f5e0ff70ab
Vadosmik <wadimmikanowicz@gmail.com>
2026-05-10 12:59:28 +0200

    some changes in config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite'
 
 export default defineConfig({
   plugins: [preact()],
-  base: '/Lorely/frontend/',
+  base: '/Lorely/frontend/dist',
   resolve: {
     alias: {
       'react': 'preact/compat',
