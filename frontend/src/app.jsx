81334a791b18822920d61ed8068fdffd797f31bc
Vadosmik <wadimmikanowicz@gmail.com>
2026-05-10 13:46:28 +0200

    del dist

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,13 +1,17 @@
 import { LocationProvider, Router, Route } from 'preact-iso';
 import EditorPage from './views/editor/EditorPage';
-import { ListView } from './views/editor/ListView';
+// import ListView from './views/editor/ListView'; // Removed ListView import
+// import ProfilePge from './views/profile/ProfilePge';
+
+// Jeśli preact-iso nie obsługuje base path wprost,
+// musisz dodać bazę do ścieżek:
 
 export function App() {
   const base = '/Lorely/frontend';
   return (
     <LocationProvider>
       <Router>
-        <Route path={`${base}/`} component={ListView} />
+        <Route path={`${base}/`} component={EditorPage} /> {/* Render EditorPage at root */}
         <Route path={`${base}/editor`} component={EditorPage} />
         <Route path={`${base}/editor/:id`} component={EditorPage} />
       </Router>
